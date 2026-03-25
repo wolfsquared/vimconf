a19467038c48b35cfea6e929c8869099d8b3767d
Swanepoel <Andre.Swanepoel2@standardbank.co.za>
2026-03-25 18:51:26 +0200

    fixed broken treesitter

--- init.lua
+++ init.lua
@@ -901,7 +901,7 @@ require('lazy').setup({
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
       ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+      require('nvim-treesitter').setup(opts)
 
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
